3f1a20ba1136ae8962078030b4cfa092985ceaf4
sitati-elsis <sitatielsis@gmail.com>
2023-12-20 17:06:52 +0300

    Add a create method to PlaneViewset - Add a create method that will respond to requests on the "/planes/" endpoint when the POST method is used.

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,12 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 
-from api import models
-from api import serializers
+from api import serializers, utils
+
 
 
 class PlaneViewset(viewsets.ViewSet):
+    def create(self, request):
+        serializer = serializers.PlaneSerializer(data=request.data)
+        if serializer.is_valid():
+            utils.create_plane_entry(serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def list(self, request):
         queryset = models.Plane.objects.all()
         serializer = serializers.PlaneSerializer(queryset, many=True)
