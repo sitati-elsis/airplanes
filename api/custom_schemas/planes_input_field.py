f674be39388d567a6bc2eae6e5960db1a09999be
sitati-elsis <sitatielsis@gmail.com>
2023-12-20 22:33:58 +0300

    Customize the POST /planes/ request body schema - Define expected input schema for the request body. This should make the interactive documentation more user friendly by indicating expected fields and their expected datatypes.

---
+++ api/custom_schemas/planes_input_field.py
@@ -0,0 +1,27 @@
+from rest_framework.schemas.openapi import SchemaGenerator
+
+class PlanesInputSchema(SchemaGenerator):
+    """
+    Defines expected Schema structure when adding planes from the interactive
+    documentation
+    """
+    def get_schema(self, *args, **kwargs):
+        schema = super().get_schema(*args, **kwargs)
+        schema_structure = {
+            'type': 'object',
+            'properties': {
+                'id': {
+                    'type': 'integer',
+                    'format': 'int32',
+                    'description': 'The Plane id',
+                },
+                'passengers': {
+                    'type': 'integer',
+                    'format': 'int32',
+                    'description': 'The number of passengers',
+                }
+            },
+        }
+        schema['paths']['/api/planes/']['post']['requestBody']['content']\
+            ['application/json']['schema'] = schema_structure
+        return schema
