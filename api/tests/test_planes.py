3351daede1218920e6f3c0ba0d56c4bb4251c4e7
sitati-elsis <sitatielsis@gmail.com>
2023-12-20 20:21:06 +0300

    Test plane entry must have required fields - Test that a plane entry cannot be made without having all required fields i.e. `id` and `passengers`.

--- api/tests/test_planes.py
+++ api/tests/test_planes.py
@@ -72,3 +72,28 @@ class TestPlanes(APITestCase):
         # assert that no plane entry was made in the database
         planes = models.Plane.objects.all()
         self.assertEqual(len(planes), 0)
+
+    def test_cannot_create_plane_entry_without_id_and_passengers_field(self):
+        """
+        Tests that a plane entry cannot be made without both required fields
+        `id` and `passengers`.
+        """
+        # assert that database contains no Plane objects
+        planes = models.Plane.objects.all()
+        self.assertEqual(len(planes), 0)
+        # attempt to create plane entry
+        data = {
+            'random_field_1': self.fake.random_int(max=32767),
+            'random_field_2': self.fake.random_int(max=32767),
+        }
+        url = reverse('planes-list')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 400)
+        json_response = response.json()
+        id_error = json_response['id'][0]
+        passenger_error = json_response['passengers'][0]
+        self.assertIn('This field is required', id_error)
+        self.assertIn('This field is required', passenger_error)
+        # assert that no plane entry was made in the database
+        planes = models.Plane.objects.all()
+        self.assertEqual(len(planes), 0)
